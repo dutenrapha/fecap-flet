6eb1f6350ee58b805a58892cee3bf501bb9b42f2
Gabriel Ferrari Guido de Lima <22011031@fecap.br>
2024-03-07 11:07:57 -0300

    alteraçoes

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ class SentimentRequest(BaseModel):
 async def get_sentiment(sentiment_request: SentimentRequest):
     sentiment = random.choice(["positivo", "negativo"])
     return {"sentiment": sentiment}
-
+#gabriel ferrari
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
